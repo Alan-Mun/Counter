9cc28a0e5a4ce99f74826089e8d48320aba8f060
Alan-Mun <alan.mun.98@mail.ru>
2023-10-23 19:26:17 +0300

    Базовая версия решения задачи

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterText: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterText.text = "0"
+        counterButton.setTitle("Нажми", for: .normal)
     }
 
-
+    var counterNumbers: Int = 0
+    
+    @IBAction func buttonDidTap(_ sender: UIButton) {
+        self.counterNumbers += 1
+        self.counterText.text = "Значение счётчика: \(self.counterNumbers)"
+    }
+    
 }
 
