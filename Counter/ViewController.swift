623cb799556624135ed95a76d5f37e992fa526e9
Alan-Mun <alan.mun.98@mail.ru>
2023-10-24 20:19:59 +0300

    Усложненная версия

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,20 +10,78 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var counterText: UILabel!
-    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterPlusButton: UIButton!
+    @IBOutlet weak var counterZeroButton: UIButton!
+    @IBOutlet weak var counterMinusButton: UIButton!
+    @IBOutlet weak var textHistoryBox: UITextView!
+    
+    var counterNumbers: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         counterText.text = "0"
-        counterButton.setTitle("Нажми", for: .normal)
+        
+        textHistoryBox.text = "История изменений:"
+        textHistoryBox.textAlignment = .center
+        textHistoryBox.layer.cornerRadius = 20
+        textHistoryBox.layer.masksToBounds = true
+        textHistoryBox.isEditable = false
+        textHistoryBox.isScrollEnabled = true
+        
+        counterPlusButton.setImage(UIImage(systemName: "plus.square"), for: .normal)
+        counterPlusButton.tintColor = .red
+        
+        counterZeroButton.setImage(UIImage(systemName: "multiply.circle"), for: .normal)
+        counterZeroButton.tintColor = .green
+        
+        counterMinusButton.setImage(UIImage(systemName: "minus.square"), for: .normal)
+        counterMinusButton.tintColor = .blue
     }
-
-    var counterNumbers: Int = 0
     
-    @IBAction func buttonDidTap(_ sender: UIButton) {
-        self.counterNumbers += 1
-        self.counterText.text = "Значение счётчика: \(self.counterNumbers)"
+    @IBAction func counterPlusDidTap(_ sender: Any) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+
+        let formattedDate = dateFormatter.string(from: Date())
+        
+        counterNumbers = counterNumbers + 1
+        counterText.text = "Значение счётчика: \(counterNumbers)"
+        
+        textHistoryBox.text = "\(textHistoryBox.text!) \n[\(formattedDate)]\n значение изменено на +1"
+    }
+    
+    @IBAction func counterZeroDidTap(_ sender: Any) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+
+        let formattedDate = dateFormatter.string(from: Date())
+        
+        counterNumbers = 0
+        counterText.text = "Значение счётчика: \(counterNumbers)"
+        textHistoryBox.text = "\(textHistoryBox.text!) \n[\(formattedDate)]\n значение сброшено"
+        
+    }
+    
+    @IBAction func counterMinusDidTap(_ sender: Any) {
+       
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+
+        let formattedDate = dateFormatter.string(from: Date())
+        
+        if counterNumbers > 0 {
+            counterNumbers = counterNumbers - 1
+            counterText.text = "Значение счётчика: \(counterNumbers)"
+            textHistoryBox.text = "\(textHistoryBox.text!) \n[\(formattedDate)]\n значение изменено на -1"}
+        else { counterNumbers = 0
+            textHistoryBox.text = "\(textHistoryBox.text!) \n[\(formattedDate)]\n попытка уменьшить значение\nсчётчика ниже 0"
+        }
     }
     
 }
